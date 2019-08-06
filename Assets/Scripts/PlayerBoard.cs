44e7e78ae5d4dbf59842315c90311fd7f33f492e
Hyrybydysz <wsokolnicki93@gmail.com>
2019-08-06 22:03:57 +0200

    Knight Board Update
    First steps, first ideas how I would like to see board managment in the game.
    Clicking on the board it will zoom towards it, then you can press RMB on text to enlarge it  if necessary.

--- Assets/Scripts/PlayerBoard.cs
+++ Assets/Scripts/PlayerBoard.cs
@@ -13,7 +13,8 @@ public class PlayerBoard : MonoBehaviour
     Vector3 cameraStartPosition;
     Vector3 cameraStartRotation;
     float cameraAngleY;
-    [SerializeField] bool zoomedIn = false;
+    bool zoomedIn = false;
+    bool infoClicked = false;
 
     [Header("BOARD INFO CACHE", order = 2)]
     [SerializeField] GameObject colliders;
@@ -23,7 +24,41 @@ public class PlayerBoard : MonoBehaviour
     [Header("_Bow", order = 4)]
     [SerializeField] GameObject bowCollider;
     [SerializeField] GameObject bowPanel;
+    [Header("_Map", order = 5)]
+    [SerializeField] GameObject mapCollider;
+    [SerializeField] GameObject mapPanel;
+    [Header("_Shield", order = 6)]
+    [SerializeField] GameObject shieldCollider;
+    [SerializeField] GameObject shieldPanel;
+    [Header("_TheKnight", order = 7)]
+    [SerializeField] GameObject knightCollider;
+    [SerializeField] GameObject knightPanel;
+    [Header("_Grit", order = 8)]
+    [SerializeField] GameObject gritCollider;
+    [SerializeField] GameObject gritPanel;
 
+    GameObject currentlyOpenPanel;
+
+    GameObject ReturnPanel(string name)
+    {
+        switch(name)
+        {
+            case "bomb":
+                return bombPanel;
+            case "bow":
+                return bowPanel;
+            case "map":
+                return mapPanel;
+            case "shield":
+                return shieldPanel;
+            case "knight":
+                return knightPanel;
+            case "grit":
+                return gritPanel;
+            default:
+                return null;
+        }
+    }
 
     void Start()
     {
@@ -61,7 +96,7 @@ public class PlayerBoard : MonoBehaviour
                     TurnSelectedChilderON_OFF(true);
                 }
                 else if (hit.collider.gameObject == bombCollider || 
-                    hit.collider.gameObject == bowCollider)
+                    hit.collider.gameObject == bowCollider || infoClicked)
                     return;
                 else
                 {
@@ -82,33 +117,67 @@ public class PlayerBoard : MonoBehaviour
 
     void BoardInfo()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && !infoClicked)
         {
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == bombCollider)
+            if (Physics.Raycast(ray, out hit))
             {
-                bombPanel.SetActive(true);
-                EnlargeImages(bombPanel.transform.GetChild(0), bombPanel.transform.GetChild(1));
+                if (hit.collider.gameObject == bombCollider)
+                    PanelInfo("bomb");
+                else if (hit.collider.gameObject == bowCollider)
+                    PanelInfo("bow");
+                else if (hit.collider.gameObject == mapCollider)
+                    PanelInfo("map");
+                else if (hit.collider.gameObject == shieldCollider)
+                    PanelInfo("shield");
+                else if (hit.collider.gameObject == knightCollider)
+                    PanelInfo("knight");
+                else if (hit.collider.gameObject == gritCollider)
+                    PanelInfo("grit");
             }
-
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == bowCollider)
-            {
-                bowPanel.SetActive(true);
-            }
+        }
+        else if(Input.GetMouseButton(0) && infoClicked)
+        {
+            infoClicked = false;
+            TurnPanelOff(currentlyOpenPanel);
+            currentlyOpenPanel = null;
         }
     }
 
-    void EnlargeImages(Transform image_1, Transform image_2)
+    void PanelInfo(string name)
+    {
+        GameObject panel = ReturnPanel(name);
+        currentlyOpenPanel = panel;
+        panel.SetActive(true);
+        infoClicked = true;
+        StartCoroutine(EnlargeImages(panel.transform.GetChild(0), panel.transform.GetChild(1), name));
+    }
+    void TurnPanelOff(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+    IEnumerator EnlargeImages(Transform image_1, Transform image_2, string animationTrigger)
     {
         RectTransform image1 = image_1.GetComponent<RectTransform>();
         RectTransform image2 = image_2.GetComponent<RectTransform>();
 
-        image1.sizeDelta = image1.sizeDelta * 3;
-        image1.localPosition = new Vector2(0, image1.sizeDelta.y / 2);
-       
-        image2.sizeDelta = image2.sizeDelta * 3;
-        image2.localPosition = new Vector2(0, -image2.sizeDelta.y /2);
+        Vector2 image1StartSize = image1.sizeDelta;
+        Vector3 image1StartPosition = image1.localPosition;
+        Vector2 image2StartSize = image2.sizeDelta;
+        Vector3 image2StartPosition = image2.localPosition;
+
+        image_1.parent.parent.GetComponent<Animator>().SetTrigger(animationTrigger);
+
+        yield return new WaitUntil(() => !infoClicked);
+
+        image_1.parent.parent.GetComponent<Animator>().SetTrigger("back");
+
+        image1.sizeDelta = image1StartSize;
+        image1.localPosition = image1StartPosition;
+
+        image2.sizeDelta = image2StartSize;
+        image2.localPosition = image2StartPosition;
     }
 }
